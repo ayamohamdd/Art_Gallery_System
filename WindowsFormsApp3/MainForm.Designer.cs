b59ac9bd60e54b5304cbfafa84d370ecfc410c91
Aya <maya30315@gmail.com>
2024-05-05 14:19:22 +0300

    remove .vs

--- WindowsFormsApp3/MainForm.Designer.cs
+++ WindowsFormsApp3/MainForm.Designer.cs
@@ -30,6 +30,8 @@ namespace WindowsFormsApp3
         private void InitializeComponent()
         {
             this.loadButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // loadButton
@@ -43,11 +45,35 @@ namespace WindowsFormsApp3
             this.loadButton.UseVisualStyleBackColor = true;
             this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
             // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(370, 145);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(365, 41);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Connected Mode";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(370, 239);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(365, 41);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Crystal Report";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1230, 727);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.loadButton);
             this.Name = "MainForm";
             this.Text = "MainForm";
@@ -58,6 +84,8 @@ namespace WindowsFormsApp3
 
         #endregion
         private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
